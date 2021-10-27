43bc36f421cbe7c1b277e90f25a800b0e1edefc3
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-27 15:32:48 +0200

    consegna esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('ciao');
+/*
+Dati tre array contenenti:
+ - una lista ordinata di 5 immagini,
+ - una lista ordinata dei relativi 5 luoghi e
+ - una lista di 5 news,
+creare un carosello come nella foto allegata.
+
+Al click dell’utente sulle frecce verso l’alto o verso il basso, l’immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi:
+ - titolo
+ - testo.
+
+ Allo stesso tempo nelle miniature l’immagine attiva dovrà apparire in evidenza rispetto alle altre.
+*/
+
+/*
+1. costruiamo del carosello una versione statica contenente un’immagine grande con del testo ben posizionato e una miniatura. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come “template”.
+
+2. scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+
+3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: “Quanti cicli servono?”
+*/
